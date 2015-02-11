44c7fbcaaca4fd0291232761b364a0ef3fcdd19a
Debbie Cravey and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-02-11 10:46:16 -0800

    add ability to enter nickels, pennies, and big numbers

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,12 @@ var coinCombo = function(totalCents) {
   totalCents = totalCents - (numberOfQuarters * 25);
   var numberOfDimes = Math.floor(totalCents / 10);
   totalCents = totalCents - (numberOfDimes * 10);
-
+  var numberOfNickles = Math.floor(totalCents / 5);
+  totalCents = totalCents - (numberOfNickles * 5);
 
   results[0] = numberOfQuarters;
   results[1] = numberOfDimes;
+  results[2] = numberOfNickles;
+  results[3] = totalCents;
   return results;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,24 @@ describe('coinCombo', function() {
   it("returns 1 quarter and 1 dime if user input is 35", function() {
     expect(coinCombo(35)).to.eql([1,1,0,0]);
   });
+
+  it("returns 1 quarter, 1 dime, and 1 nickel if user input is 40", function() {
+    expect(coinCombo(40)).to.eql([1,1,1,0]);
+  });
+
+  it("returns 1 quarter, 1 dime, 1 nickle, 1 penny if user input is 41", function(){
+    expect(coinCombo(41)).to.eql([1,1,1,1]);
+  });
+
+  it("returns 1 dime if user input is 10", function() {
+    expect(coinCombo(10)).to.eql([0,1,0,0]);
+  });
+
+  it("returns 1 nickel if user input is 5", function() {
+    expect(coinCombo(5)).to.eql([0,0,1,0]);
+  });
+
+  it("returns the correct change combination if user input is 532", function() {
+    expect(coinCombo(532)).to.eql([21, 0, 1, 2]);
+  });
 });
